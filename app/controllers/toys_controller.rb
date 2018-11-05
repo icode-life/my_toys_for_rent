3130c63226bea811252116f295ebbdf1dab511be
Tarig Bouazzati <tbouzza@gmail.com>
2018-11-05 21:09:41 +0100

    updated sign up requirements

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,4 +1,6 @@
 class ToysController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_toy, only: [:show, :edit, :update, :destroy]
 
   def index
     if params[:search].blank?
@@ -26,5 +28,17 @@ class ToysController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_restaurant
+    @toys = Toy.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def toys_params
+    params.require(:toys).permit(:name, :description, :category, :price, :owner_id)
   end
 end
