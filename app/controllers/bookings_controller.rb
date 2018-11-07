bc4d8d848d650b93a3bc45154b5a7eff015c1364
Andrzej Gbur <andrzej.gbur@yahoo.com>
2018-11-07 11:42:07 +0100

    toy create fix admin default added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,6 @@
 class BookingsController < ApplicationController
-<<<<<<< HEAD
-  before_action :find_booking, except: [:index, :new, :create]
-
-=======
   before_action :find_booking, only: [:show, :edit, :update, :destroy]
   after_action :verify_policy_scoped, only: :index
->>>>>>> f5a09acf58a8fee108fa25535a0caf3c1b4e1ba6
 
   def index
     @bookings = policy_scope(Booking).order(created_at: :desc)
