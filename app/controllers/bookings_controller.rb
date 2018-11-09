ed2215ca82c4b10b5d936b27119551386e6979ef
Andrzej Gbur <andrzej.gbur@yahoo.com>
2018-11-09 07:54:11 +0100

    Fixed and updated: User profile - My Bookings & My inventory

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -23,7 +23,7 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     authorize @booking
     if @booking.save
-      redirect_to bookings_path
+      redirect_to user_path(current_user)
     else
       render :new
     end
