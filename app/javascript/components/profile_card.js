19c9f03191b168ebb45ea9b82dc2556d912797f5
Andrzej Gbur <andrzej.gbur@yahoo.com>
2018-11-09 19:38:33 +0100

    babel files deleted

--- app/javascript/components/profile_card.js
+++ app/javascript/components/profile_card.js
@@ -1,5 +1,5 @@
 function profileCard() { $(document).ready(function() {
-  $(".btn-pref .btn").click(function () {
+  $(".btn-pref1 .btn").click(function () {
       console.log("hi");
       $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
       // $(".tab").addClass("active"); // instead of this do the below
